44e36f5446fb93cba2ef5abb9aefb28b201e4267
coding <coding@coding.net>
2020-10-28 14:45:45 +0000

    sdf

--- src/main/java/org/yeauty/pojo/DiscoverConfigBO.java
+++ src/main/java/org/yeauty/pojo/DiscoverConfigBO.java
@@ -1,46 +1,39 @@
 package org.yeauty.pojo;
-
 public class DiscoverConfigBO {
-
     private String configPath;
-    private String proxyPass;
+    private String proxyPassd;
     private String serviceName;
-
     public DiscoverConfigBO(String configPath, String proxyPass, String serviceName) {
         this.configPath = configPath;
         this.proxyPass = proxyPass;
         this.serviceName = serviceName;
     }
-
     @Override
     public String toString() {
         return "DiscoverConfigBO{" +
-                "configPath='" + configPath + '\'' +
-                ", proxyPass='" + proxyPass + '\'' +
-                ", serviceName='" + serviceName + '\'' +
-                '}';
+            "configPath='" + configPath + '\'' +
+            ", proxyPass='" + proxyPass + '\'' +
+            ", serviceName='" + serviceName + '\'' +
+            '}';
     }
-
+    public String getCode(){
+     System.out.println(0)
+    }
     public String getConfigPath() {
         return configPath;
     }
-
     public void setConfigPath(String configPath) {
         this.configPath = configPath;
     }
-
     public String getProxyPass() {
         return proxyPass;
     }
-
     public void setProxyPass(String proxyPass) {
         this.proxyPass = proxyPass;
     }
-
     public String getServiceName() {
         return serviceName;
     }
-
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
     }
